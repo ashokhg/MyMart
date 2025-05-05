dc24fdc0ab6c53b062c9724162226b6ddb00d59c
Ashok kumar <ashoka4599@gmail.com>
2025-05-05 23:00:16 +0530

    added sprig security

---
+++ user-service/src/main/java/com/mymart/userservice/filters/JwtFilter.java
@@ -0,0 +1,9 @@
+package com.mymart.userservice.filters;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtFilter {
+
+	
+}
---
+++ user-service/src/main/java/com/mymart/userservice/service/JwtServiceImpl.java
@@ -0,0 +1,9 @@
+package com.mymart.userservice.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtServiceImpl {
+
+	private String secretKey;
+}
