2f8fc52548f59fd0c9497d1a367e1cc204526732
Ashok kumar <ashoka4599@gmail.com>
2025-05-06 22:11:05 +0530

    creating jwt generation, auth

--- user-service/src/main/java/com/mymart/userservice/config/SecurityConfig.java
+++ user-service/src/main/java/com/mymart/userservice/config/SecurityConfig.java
@@ -3,9 +3,11 @@ package com.mymart.userservice.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -14,12 +16,17 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import com.mymart.userservice.filters.JwtFilter;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
 	@Autowired
 	private UserDetailsService userDetailsService;
+	
+	@Autowired
+	JwtFilter jwtFilter;
 	
 	@Bean
 	public AuthenticationProvider authenticationProvider() {
@@ -43,5 +50,10 @@ public class SecurityConfig {
 		http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 		http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
 		return http.build();
+	}
+	
+	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+		return authenticationConfiguration.getAuthenticationManager();
 	}
 }
--- user-service/src/main/java/com/mymart/userservice/filters/JwtFilter.java
+++ user-service/src/main/java/com/mymart/userservice/filters/JwtFilter.java
@@ -1,9 +1,41 @@
 package com.mymart.userservice.filters;
 
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.mymart.userservice.service.JwtServiceImpl;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @Component
-public class JwtFilter {
+public class JwtFilter extends OncePerRequestFilter{
+
+	@Autowired
+	JwtServiceImpl jwtServiceImpl;
+	
+	@Autowired
+	ApplicationContext applicationContext;
+	
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+			throws ServletException, IOException {
+
+		String authHeader = request.getHeader("Authorization");
+		String token = null;
+		String userName = null;
+		
+		if(authHeader != null && authHeader.startsWith("Bearer ")) {
+			token = authHeader.substring(7);
+			userName = jwtServiceImpl.extractUserName(token);
+		}
+	}
 
 	
 }
